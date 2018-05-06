d24a6bf49fc759a0e5fe974c2bb4070ce66acf73
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 01:32:55 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,4 +6,10 @@ def array_with_two_elements
   return ["Competition Chopper", "These Hands"]
 end
 
-def first_element()test_array
+def first_element(test_array)
+  return test_array[0]
+end 
+
+def third_element(test_array)
+  return test_array[2]
+end 
